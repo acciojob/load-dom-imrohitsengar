d4d1abec5205a5d16073f139aeee9ce9ab86e6aa
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-05 10:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let body=document.querySelector('body');
-body.addEventListener('DOMContentLoaded',e =>{
+document.addEventListener('DOMContentLoaded',e =>{
 	let content=document.createElement('p');
 	content.textContent="DOM load sucess";
 	body.appendChild(content);
